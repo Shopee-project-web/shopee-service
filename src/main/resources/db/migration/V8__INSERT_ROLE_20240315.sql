3633acae807c0adb3a70b8e1c6987d1ba039e1ec
sn0509 <144189061+sn0509@users.noreply.github.com>
2024-03-15 13:52:58 +0700

    BE: 1: complete logout. 2.insert role

---
+++ src/main/resources/db/migration/V8__INSERT_ROLE_20240315.sql
@@ -0,0 +1,3 @@
+INSERT INTO `shoppify_data`.`role` (`NAME`) VALUES ('ROLE_ADMIN');
+INSERT INTO `shoppify_data`.`role` (`NAME`) VALUES ('ROLE_CUSTOMER');
+INSERT INTO `shoppify_data`.`role` (`NAME`) VALUES ('ROLE_SELLER');
