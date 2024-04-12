9d3dedd612eaf6db3ebf319011bdbbaa0a4d925f
tiennguyen-developer <tiennguyen.building@gmail.com>
2024-04-12 14:10:45 +0700

    demo

--- src/main/resources/db/migration/V9__INSERT_USER_20240315.sql
+++ src/main/resources/db/migration/V9__INSERT_USER_20240315.sql
@@ -2,7 +2,6 @@
 INSERT INTO `shopee_data`.`user` (`PHONE_NUMBER`, `USERNAME`, `EMAIL`, `PASSWORD`, `IS_DELETED`) VALUES ('0909123456', 'daonguyen123', 'daonguyen123@gmail.com', '$2a$10$Q1lkb6N6eS7VGcstaXsy1eVHRnXMY0SRqqOepSiHXR7iutXtEFPHq', '0');
 INSERT INTO `shopee_data`.`user` (`PHONE_NUMBER`, `USERNAME`, `EMAIL`, `PASSWORD`, `IS_DELETED`) VALUES ('0909778899', 'tiennguyen123', 'tiennguyen123@gmail.com', '$2a$10$UVtRMGvkXi6zpJ5j19Lv2eyYt78YHXjRfOxajy8v0TH8hYMAUIgvK', '0');
 
-
 -- Insert User_Role
 INSERT INTO `shopee_data`.`user_role` (`USER_ID`, `ROLE_ID`) VALUES ('1', '1');
 INSERT INTO `shopee_data`.`user_role` (`USER_ID`, `ROLE_ID`) VALUES ('1', '2');
